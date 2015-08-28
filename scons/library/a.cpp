af8111e3ab4771e728b7aaabecec7dfac6383001
Adam Hlavatovic <adam.hlavatovic@gmail.com>
2015-08-28 15:04:55 +0200

    scons: build a library

---
+++ scons/library/a.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "b.hpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "The version is: " << version() << "\n";
+	return 0;
+}	
---
+++ scons/library/b.cpp
@@ -0,0 +1,6 @@
+#include "b.hpp"
+
+std::string version()
+{
+	return "1.0.0.1";
+}
