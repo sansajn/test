31aaf53cb749bb9faca972efa4592be8804c110e
Adam Hlavatovic <sansajn@ymail.com>
2015-12-29 15:51:01 +0100

    log like streambuf

--- io/log_streambuf.cpp
+++ io/log_streambuf.cpp
@@ -1,75 +1,117 @@
 #include <streambuf>
 #include <iostream>
+#include <cassert>
 
-class log_streambuf : public std::streambuf
+// vlozenie kazdeho znaku sposoby overflow() (co je pomale oproti priamemu volaniu log funkcie)
+class log_overflow : public std::streambuf
 {
 public:
-	log_streambuf(int log_prio);
-	~log_streambuf();
+	log_overflow() : _cur{_buf} {}
+	~log_overflow() {flush();}
 
 protected:
-	int_type overflow(int_type c) override;  //!< buffer je plny, vyprazdni ho
-	int sync() override;
+	int_type overflow(int_type c) override  //!< buffer je plny, vyprazdni ho
+	{
+		if (c == '\n')  // znak '\n' odosle buffer do logu
+			flush();
+		else
+		{
+			if (_cur == _buf+BUFSIZE)
+				flush();
+
+			*_cur = c;
+			++_cur;
+		}
+		return c;
+	}
 
 private:
-	int_type flush_buffer();
+	void flush()
+	{
+		if (_cur == _buf)
+			return;
+
+		std::cout << "LOG:";
+		std::cout.write(_buf, _cur - _buf);
+		std::cout << std::endl;
+		_cur = _buf;
+	}
 
 	static int const BUFSIZE = 1024;
 	char _buf[BUFSIZE];
-	int _prio;
+	char * _cur;
 };
 
 
-log_streambuf::log_streambuf(int log_prio) : _prio{log_prio}
+// do logu odosle buffer ak poslednym znakom je znak '\n'
+class log_sync : public std::streambuf
 {
-	setp(_buf, _buf + BUFSIZE-2);  // rezervuj si 2 znaky na overflow znak a znak '\0'
-}
+public:
+	log_sync() {setp(_buf, _buf + BUFSIZE-1);}
+	~log_sync() {flush();}
 
-log_streambuf::~log_streambuf()
-{
-	sync();
-}
-
-log_streambuf::int_type log_streambuf::overflow(int_type c)
-{
-	if (c != EOF)
+protected:
+	int sync() override
 	{
-		*pptr() = c;
-		pbump(1);
+		if (*(pptr()-1) == '\n')
+			flush();
+		return 0;
 	}
 
-	if (flush_buffer() == EOF)
-		return EOF;
+	int_type overflow(int_type c) override
+	{
+		assert(0 && "buffer overflow");
+		flush();
+		setp(_buf, _buf + BUFSIZE-1);
+		return c;
+	}
 
-	return c;
+private:
+	void flush()
+	{
+		if (pptr() == _buf)
+			return;
+
+		std::cout << "LOG:";
+		std::cout.write(_buf, pptr() - pbase() - 1);  // ignore '\n'
+		std::cout << std::endl;
+		setp(_buf, _buf + BUFSIZE-1);
+	}
+
+	static int const BUFSIZE = 1024;
+	char _buf[BUFSIZE];
+};
+
+
+void test_log_overflow()
+{
+	log_overflow out;
+	std::streambuf * cerr_buff = std::cerr.rdbuf();
+	std::cerr.rdbuf(&out);
+
+	int ival = 3;
+	std::cerr << "hello " << "lisbon, " << ival << "x times" << std::endl;
+
+	std::cerr.rdbuf(cerr_buff);
 }
 
-log_streambuf::int_type log_streambuf::flush_buffer()
+void test_log_sync()
 {
-	int num = pptr() - pbase();
-	if (num > 0)
-		std::cout.write(_buf, num);
-	pbump(-num);  // setp(_buf, _buf + MAX_BUFSIZE-2);
-	return num;
-}
+	log_sync out;
+	std::streambuf * cerr_buff = std::cerr.rdbuf();
+	std::cerr.rdbuf(&out);
 
-int log_streambuf::sync()
-{
-	return flush_buffer() != EOF ? 0 : -1;
+	int ival = 3;
+	std::cerr << "hello " << "lisbon, " << ival << "x times" << std::endl;
+
+	std::cerr.rdbuf(cerr_buff);
 }
 
 
 int main(int argc, char * argv[])
 {
-	log_streambuf out{0};
-	std::streambuf * cerr_buff = std::cerr.rdbuf();
-	std::cerr.rdbuf(&out);
-
-	std::cerr << "hello" << std::endl;
-	std::cerr << "lisbon" << std::endl;
-
-
-	std::cerr.rdbuf(cerr_buff);
+//	test_log_overflow();
+	test_log_sync();
 
 	return 0;
 }
