898fb33049a2084aadaeea4dc957a172789b25f4
Adam Hlavatovic <sansajn@ymail.com>
2016-01-15 12:06:41 +0100

    linux: signal handling

---
+++ linux/sighandler.cpp
@@ -0,0 +1,37 @@
+// chitanie signalou pri pade aplykacie
+#include <iostream>
+#include <execinfo.h>
+// posix
+#include <signal.h>
+#include <unistd.h>
+
+void print_stack_trace()
+{
+	void * array[30];
+	size_t size = backtrace(array, 30);
+	backtrace_symbols_fd(array, size, STDERR_FILENO);
+}
+
+void custom_handler(int signum)
+{
+	print_stack_trace();
+	std::abort();
+}
+
+struct foo 
+{
+	void func() {
+		std::cout << "foo::func():{n=}" << _n << std::endl;
+	}
+	
+	int _n;
+};
+
+int main(int argc,char * argv[])
+{
+	signal(SIGSEGV, custom_handler);
+	
+	foo * f = nullptr;
+	f->func();  // Segmentation fault (core dumped)
+	return 0;
+}
