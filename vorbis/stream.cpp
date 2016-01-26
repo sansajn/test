3d7f2dbb6056a1cc44c386fedefc163117b34dcb
Adam Hlavatovic <sansajn@ymail.com>
2016-01-26 18:22:40 +0100

    vorbis: stream (random access)

--- vorbis/stream.cpp
+++ vorbis/stream.cpp
@@ -15,6 +15,11 @@ constexpr int BUFFER_TIME_MS = 200;
 
 using std::cout;
 
+size_t vorbis_read(OggVorbis_File & vf, uint8_t * data, size_t size, int bits_per_sample);
+size_t vorbis_read_samples(OggVorbis_File & vf, uint8_t * data, size_t sample_count, int bits_per_sample);
+size_t vorbis_read_samples(OggVorbis_File & vf, size_t offset, uint8_t * data, size_t sample_count, int sample_width, int channels);
+
+
 int main(int argc, char * argv[])
 {
 	// device & context
@@ -35,7 +40,8 @@ int main(int argc, char * argv[])
 	assert(ret == 0 && "ov_open() failed");
 
 	vorbis_info * vi = ov_info(&vf, -1);
-	int bits_per_sample = 16;  // konvertovane s float-u
+	int sample_width = 2;  // konvertovane s float-u
+	int sample_size = sample_width * vi->channels;
 
 	cout << "channels:" << vi->channels << ", freq:" << vi->rate << "\n";
 	cout << "length:" << ov_pcm_total(&vf, -1) << " samples\n";
@@ -48,30 +54,24 @@ int main(int argc, char * argv[])
 	// buffers
 	ALenum format;
 	if (vi->channels > 1)
-		format = (bits_per_sample == 16 ? AL_FORMAT_STEREO16 : AL_FORMAT_STEREO8);
+		format = (sample_width == 2 ? AL_FORMAT_STEREO16 : AL_FORMAT_STEREO8);
 	else
-		format = (bits_per_sample == 16 ? AL_FORMAT_MONO16 : AL_FORMAT_MONO8);
+		format = (sample_width == 2 ? AL_FORMAT_MONO16 : AL_FORMAT_MONO8);
 	
 	ALuint buffers[NUM_BUFFERS];
 	alGenBuffers(NUM_BUFFERS, buffers);
-	
-	size_t buffer_size = (BUFFER_TIME_MS * vi->rate * vi->channels) / 1000;  // in samples
-	uint8_t * data = new uint8_t[buffer_size * (bits_per_sample >> 3)];
-	int current_section = 0;
+
+	size_t sample_offset = 0;
+	size_t buffer_size = (BUFFER_TIME_MS * vi->rate) / 1000;  // in samples
+	uint8_t * data = new uint8_t[buffer_size * sample_width * vi->channels];
+	cout << "buffer_size:" << buffer_size << " in samples\n";
 
 	for (int i = 0; i < NUM_BUFFERS; ++i)
 	{
-		size_t size = buffer_size;
-		size_t total_read_bytes = 0;
-		while (size > 0)
-		{
-			size_t read_bytes = ov_read(&vf, (char *)data + total_read_bytes, size, 0, bits_per_sample >> 3, 1, &current_section);
-			assert(read_bytes > 0);
-			total_read_bytes += read_bytes;
-			size -= read_bytes;
-		}
-
-		alBufferData(buffers[i], format, (ALvoid *)data, total_read_bytes, vi->rate);
+		size_t read_samples = vorbis_read_samples(vf, sample_offset, data, buffer_size, sample_width, vi->channels);
+		assert(read_samples > 0);
+		sample_offset += read_samples;
+		alBufferData(buffers[i], format, (ALvoid *)data, read_samples * sample_size, vi->rate);
 		assert(alGetError() == AL_NO_ERROR && "unable to fill a buffer");
 	}
 	
@@ -100,18 +100,11 @@ int main(int argc, char * argv[])
 		int refilled = 0;
 		for (int i = 0; i < processed; ++i, refilled += 1)
 		{
-			size_t size = buffer_size;
-			size_t total_read_bytes = 0;
-			while (size > 0)
-			{
-				size_t read_bytes = ov_read(&vf, (char *)data + total_read_bytes, size, 0, bits_per_sample >> 3, 1, &current_section);
-				if (read_bytes == 0)
-					break;  // eof
-				total_read_bytes += read_bytes;
-				size -= read_bytes;
-			}
-
-			alBufferData(removed[i], format, (ALvoid *)data, total_read_bytes, vi->rate);
+			size_t read_samples = vorbis_read_samples(vf, sample_offset, data, buffer_size, sample_width, vi->channels);
+			if (read_samples == 0)
+				break;  // eof
+			sample_offset += read_samples;
+			alBufferData(removed[i], format, (ALvoid *)data, read_samples * sample_size, vi->rate);
 		}
 		
 		alSourceQueueBuffers(source, refilled, removed);
@@ -129,4 +122,56 @@ int main(int argc, char * argv[])
 	
 	cout << "done.\n";
 	return 0;
+}
+
+size_t vorbis_read(OggVorbis_File & vf, uint8_t * data, size_t size, int bits_per_sample)
+{
+	int current_section = 0;
+	size_t total_read_bytes = 0;
+	while (size > 0)
+	{
+		size_t read_bytes = ov_read(&vf, (char *)data + total_read_bytes, size, 0, bits_per_sample >> 3, 1, &current_section);
+		if (read_bytes == 0)
+			break;  // eof
+		total_read_bytes += read_bytes;
+		size -= read_bytes;
+	}
+	return total_read_bytes;
+}
+
+size_t vorbis_read_samples(OggVorbis_File & vf, uint8_t * data, size_t sample_count, int bits_per_sample)
+{
+	int current_section = 0;
+	size_t total_read_bytes = 0;
+	size_t size = sample_count * (bits_per_sample >> 3);
+	while (size > 0)
+	{
+		size_t read_bytes = ov_read(&vf, (char *)data + total_read_bytes, size, 0, bits_per_sample >> 3, 1, &current_section);
+		if (read_bytes == 0)
+			break;  // eof
+		total_read_bytes += read_bytes;
+		size -= read_bytes;
+	}
+	return total_read_bytes / (bits_per_sample >> 3);
+}
+
+size_t vorbis_read_samples(OggVorbis_File & vf, size_t offset, uint8_t * data, size_t sample_count, int sample_width, int channels)
+{
+	cout << "pcm-tell:" << ov_pcm_tell(&vf) << ", sample-offset:" << offset << "\n";
+
+	if (offset > 0)
+		ov_pcm_seek(&vf, offset);
+
+	int current_section = 0;
+	size_t total_read_bytes = 0;
+	size_t size = sample_count * sample_width * channels;
+	while (size > 0)
+	{
+		size_t read_bytes = ov_read(&vf, (char *)data + total_read_bytes, size, 0, sample_width, 1, &current_section);
+		if (read_bytes == 0)
+			break;  // eof
+		total_read_bytes += read_bytes;
+		size -= read_bytes;
+	}
+	return total_read_bytes / (sample_width * channels);
 }
