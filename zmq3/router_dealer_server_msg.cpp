f03cb087615e72bd2a320345d7dec45af799e688
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2016-04-19 12:22:52 +0200

    zmq3: message working version of router dealer server

--- zmq3/router_dealer_server_msg.cpp
+++ zmq3/router_dealer_server_msg.cpp
@@ -1,4 +1,5 @@
 // spojenie typu ROUTER <-> DEALER, serverovska cast implementovana v zmq
+// za pouzitia zmq_msg_t struktury nameisto char* buffrou
 #include <string>
 #include <iostream>
 #include <cassert>
@@ -18,31 +19,39 @@ int main(int argc, char * argv[])
 	while (true)
 	{
 		// odpoved pride ako (identity, message)
-		char identity[1024];
-		int identity_size = zmq_recv(socket, identity, 1024, 0);
+		zmq_msg_t identity;
+		int rc = zmq_msg_init(&identity);
+		assert(rc == 0);
+		rc = zmq_msg_recv(&identity, socket, 0);
 		assert(rc != -1);
-		string s(identity, identity_size);
+		assert(rc == zmq_msg_size(&identity));
+		string s((char *)zmq_msg_data(&identity), zmq_msg_size(&identity));
 		cout << "identity: " << s << std::endl;
 
 		// zisti, ci je sprav viac
 		int more;
 		size_t option_len = sizeof(more);
-		int rc = zmq_getsockopt(socket, ZMQ_RCVMORE, &more, &option_len);
+		rc = zmq_getsockopt(socket, ZMQ_RCVMORE, &more, &option_len);
 		assert(rc == 0);
 		assert(more == 1 && "este cakam spravu");
 
 		if (more)
 		{
-			char d[1024];
-			rc = zmq_recv(socket, d, 1024, 0);
+			zmq_msg_t msg;
+			zmq_msg_init(&msg);
+			rc = zmq_msg_recv(&msg, socket, 0);
 			assert(rc != -1);
-			string s(d, rc);
+			assert(rc == zmq_msg_size(&msg));
+			string s((char *)zmq_msg_data(&msg), zmq_msg_size(&msg));
 			cout << "message: " << s << std::endl;
+			zmq_msg_close(&msg);
 		}
 
 		// answer
-		zmq_send(socket, identity, identity_size, ZMQ_SNDMORE);
+		zmq_msg_send(&identity, socket, ZMQ_SNDMORE);
 		zmq_send(socket, "world", 5, 0);
+
+		zmq_msg_close(&identity);
 	}
 
 	return 0;
