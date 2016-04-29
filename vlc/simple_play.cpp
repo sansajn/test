d651102b964d2316b0759fe6463a0c49ad6aaeb7
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2016-04-29 18:14:14 +0200

    audio and video assets

--- vlc/simple_play.cpp
+++ vlc/simple_play.cpp
@@ -1,15 +1,21 @@
 #include <thread>
+#include <string>
 #include <vlc/vlc.h>
 
-char const * default_media_path = "test.ogg";
+char const * default_media_path = "benny.ogg";
+
+using std::string;
+
 
 int main(int argc, char * argv[])
 {
+	string media_path{argc > 1 ? argv[1] : default_media_path};
+	
 	// load the vlc engine
 	libvlc_instance_t * vlc = libvlc_new(0, nullptr);
 	
 	// create a new item
-	libvlc_media_t * media = libvlc_media_new_path(vlc, default_media_path);
+	libvlc_media_t * media = libvlc_media_new_path(vlc, media_path.c_str());
 	
 	// create a media player environment
 	libvlc_media_player_t * player = libvlc_media_player_new_from_media(media);
