5455eb218b0ad2571eed238ac4549ee979b8d6cf
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2016-05-11 14:36:53 +0200

    boost::date_time: custom input facet sample

---
+++ boost/date_time/time_input_facet.cpp
@@ -0,0 +1,22 @@
+#include <sstream>
+#include <iostream>
+#include <boost/date_time.hpp>
+
+using std::cout;
+using std::stringstream;
+namespace bt = boost::posix_time;
+
+int main(int argc, char * argv[])
+{
+	std::locale custom_loc = std::locale(
+		std::locale::classic(), new bt::time_input_facet("%Y-%m-%d-%H-%M-%S"));
+	
+	bt::ptime pt;
+	stringstream is{"2016-03-14-18-12-39"};
+	is.imbue(custom_loc);
+	is >> pt;
+	
+	cout << pt << std::endl;
+	
+	return 0;
+}
