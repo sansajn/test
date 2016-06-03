26d7464ece68c7c2b69924d3f0d5b24c1cbf227d
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2016-06-03 14:29:00 +0200

    pygtk3: button sample

---
+++ python/pygtk3/button.py
@@ -0,0 +1,43 @@
+# ukazka pouzitia button widgetu
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class ButtonWindow(Gtk.Window):
+	def __init__(self):
+		Gtk.Window.__init__(self, title='Button Demo')
+		self.set_border_width(10)
+
+		hbox = Gtk.Box(spacing=6)
+		self.add(hbox)
+
+		button = Gtk.Button.new_with_label('Click Me')
+		button.connect('clicked', self.on_click_me_clicked)
+		hbox.pack_start(button, True, True, 0)
+
+		button = Gtk.Button.new_with_mnemonic('_Open')
+		button.connect('clicked', self.on_open_clicked)
+		hbox.pack_start(button, True, True, 0)
+
+		button = Gtk.Button.new_with_mnemonic('_Close')
+		button.connect('clicked', self.on_close_clicked)
+		hbox.pack_start(button, True, True, 0)
+
+	def on_click_me_clicked(self, button):
+		print("'Click me' button was clicked")
+
+	def on_open_clicked(self, button):
+		print("'Open' button was clicked")
+
+	def on_close_clicked(self, button):
+		print('Closing application')
+		Gtk.main_quit()
+
+def main():
+	win = ButtonWindow()
+	win.connect('delete-event', Gtk.main_quit)
+	win.show_all()
+	Gtk.main()
+
+if __name__ == '__main__':
+	main()
