06c4ae9f82d0b3b06182f60923ebff14e152705f
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2016-10-23 14:02:33 +0200

    regex: replace sample

--- stl/regex/replace.cpp
+++ stl/regex/replace.cpp
@@ -9,7 +9,7 @@ using std::string;
 int main(int argc, char * argv[])
 {
 	string input{"x 1 y2 22 zaq 34567"};
-	std::regex pat{R"((\w+)\s(\d))"};
+	std::regex pat{R"((\w+)\s(\d+))"};
 	string format{"{$1,$2}\n"};
 	cout << std::regex_replace(input, pat, format);
 	return 0;
