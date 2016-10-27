829ab8942011ae66d1247ab9b25e428f88e401ef
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2016-10-27 21:22:16 +0200

    boost::python: read python varible sample

---
+++ boost/python/read_variable.cpp
@@ -0,0 +1,22 @@
+// simple configuration reading sample
+#include <iostream>
+#include <boost/python.hpp>
+
+using std::cout;
+namespace py = boost::python;
+
+int main(int argc, char * argv[])
+{
+	Py_Initialize();
+
+	py::object main_module = py::import("__main__");
+	py::object main_namespace = main_module.attr("__dict__");
+	py::exec("result = 5 ** 2", main_namespace);
+	int result = py::extract<int>(main_namespace["result"]);
+	cout << "5**2=" << result << std::endl;
+
+	Py_Finalize();
+	
+	cout << "done!\n";
+	return 0;
+}
