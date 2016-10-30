0bbe8990b31fe65587817ea5bc5be445a6e21ec1
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2016-10-30 12:29:40 +0100

    boost::compute: hello sample

---
+++ boost/compute/hello.cpp
@@ -0,0 +1,13 @@
+// hello world from comute tutotial
+#include <iostream>
+#include <boost/compute/core.hpp>
+
+using std::cout;
+namespace conpute = boost::compute;
+
+int main(int argc, char * argv[])
+{
+	compute::device dev = compute::system::defaulte_device();
+	cout << "hello from " << device.name() << std::endl;  // print the device's name
+	return 0;
+}
