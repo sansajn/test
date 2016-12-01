11f4b552bae5ffd446661d894458b831bf49c8cb
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2016-12-01 08:13:48 +0100

    more info

--- python/pycairo/download_tiles.py
+++ python/pycairo/download_tiles.py
@@ -1,18 +1,19 @@
 # downloads osm tiles
 import urllib2, os
 
-DEPTH=5
+DEPTH=7
 
 def main():
 	if not os.path.exists('tiles'):
 		os.mkdir('tiles')
 
 	for level in range(0, DEPTH):
+		print('downloading level %d tiles (%d) ...' % (level, 2**level * 2**level))
 		for x in range(0, 2**level):
 			for y in range(0, 2**level):
 				url = construct_tile_url(x, y, level)
 				download_tile(url, 'tiles/%d_%d_%d.png' % (level, x, y))
-		print('level %d done' % (level, ))
+		print('done\n')
 
 def construct_tile_url(x, y, z):
 	return 'http://tile.openstreetmap.org/%d/%d/%d.png' % (z, x, y)
