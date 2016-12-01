75b53b8e6cf5c4b64d684c3199601afbeb0c07ea
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2016-12-01 16:57:01 +0100

    python embedded: extend more

--- python/embedded/extend_more.cpp
+++ python/embedded/extend_more.cpp
@@ -11,9 +11,15 @@ PyObject * emb_numargs(PyObject * self, PyObject * args)
 	return Py_BuildValue("i", numargs);
 }
 
+PyObject * emb_version(PyObject * self, PyObject * args)
+{
+	assert(!args && "null expected");
+	return Py_BuildValue("s", "0.0.1.0.0");
+}
+
 PyMethodDef emb_method_list[] = {
-	{"numargs", emb_numargs, METH_VARARGS,
-	"Return the number of arguments received by the process."},
+	{"numargs", emb_numargs, METH_VARARGS, "Return the number of arguments received by the process."},
+	{"version", emb_version, METH_NOARGS, "Return module version as a string"},
 	{NULL, NULL, 0, NULL}
 };
 
@@ -28,6 +34,8 @@ int main(int argc, char * argv[])
 	PyRun_SimpleString(
 		"import emb\n"
 		"print('Number of arguments', emb.numargs())\n"
+		"print('module version', emb.version())\n"
+		"print('done')\n"
 	);
 	
 	Py_Finalize();
