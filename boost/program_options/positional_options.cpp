1ecbb1645e279c83ed25f9f05dd7c02f4d168a16
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2017-01-17 13:18:24 +0100

    boost::program_options: positional options sample

---
+++ boost/program_options/positional_options.cpp
@@ -0,0 +1,42 @@
+// positional options handling
+#include <string>
+#include <iostream>
+#include <boost/program_options.hpp>
+
+using std::string;
+using std::cout;
+namespace po = boost::program_options;
+
+int main(int argc, char * argv[])
+{
+	po::options_description desc{"Allowed options"};
+	desc.add_options()
+		("help", "produce help messages")
+		("compression", po::value<int>(), "set complession level")
+		("url", po::value<string>(), "startup page");
+
+	po::positional_options_description pos_desc;
+	pos_desc.add("url", 1);
+
+	po::variables_map vm;
+	po::store(po::command_line_parser(argc, argv).options(desc).positional(pos_desc).run(), vm);
+	po::notify(vm);  // to je naco (zrejme nejake call-backy) ?
+
+	if (vm.count("help"))
+	{
+		cout << desc << "\n";
+		return 1;
+	}
+
+	if (vm.count("compression"))
+		cout << "Compression level was set to " << vm["compression"].as<int>() << ".\n";
+	else
+		cout << "Compression leel was not set.\n";
+	
+	if (vm.count("url"))
+		cout << "url set to '" << vm["url"].as<string>() << "\n";
+	else
+		cout << "url was not set.\n";
+
+	return 0;
+}
