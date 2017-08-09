47dfe9f8328e182930e17a8732f38aea0ea92da8
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2017-08-09 09:36:13 +0200

    jpeg reencode using only libjpeg library sample

--- jpeg/reencode.cpp
+++ jpeg/reencode.cpp
@@ -3,21 +3,21 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <csetjmp>
 #include <jpeglib.h>
 #include <boost/program_options.hpp>
-#include <Magick++.h>
 
 using std::string;
 using std::cout;
 using std::pair;
 using std::make_pair;
-using namespace Magick;
 namespace po = boost::program_options;
 
-// returns image geometry and blob image representation
-static pair<size_t, size_t> im_read(string const & fname, Blob & data);
-
 // reencode [options] INPUT OUTPUT
+
+uint8_t * read_image(string const & filename, size_t & w, size_t & h);
+void write_image(uint8_t * pixels, size_t w, size_t h, int q, string const & filename);
+
 
 int main(int argc, char * argv[])
 {
@@ -48,13 +48,62 @@ int main(int argc, char * argv[])
 		output_filename = argv[argc-1];
 	}
 
-	// prepare input data
-	Blob jpeg_data;
-	pair<size_t, size_t> geometry = im_read(input_filename, jpeg_data);
-	uint8_t * image_buffer = (uint8_t *)jpeg_data.data();
-	size_t const & image_width = geometry.first;
-	size_t const & image_height = geometry.second;
+	size_t w, h;
+	uint8_t * pixels = read_image(input_filename, w, h);
 
+	write_image(pixels, w, h, quality, output_filename);
+
+	cout << "done!\n";
+}
+
+
+uint8_t * read_image(string const & filename, size_t & w, size_t & h)
+{
+	FILE * fin = fopen(filename.c_str(), "rb");
+	assert(fin);
+
+	jpeg_decompress_struct cinfo;
+
+	// error handling
+	jpeg_error_mgr jerr;
+	cinfo.err = jpeg_std_error(&jerr);
+
+	// read jpeg
+	jpeg_create_decompress(&cinfo);
+	jpeg_stdio_src(&cinfo, fin);
+	jpeg_read_header(&cinfo, TRUE);
+	jpeg_start_decompress(&cinfo);
+
+	static_assert(BITS_IN_JSAMPLE == 8, "supports only 8-bit jpeg");
+	static_assert(std::is_same<JSAMPLE, uint8_t>::value, "JSAMPLE is not uint8_t, pixels buffer type not match");
+
+	unsigned rowbytes = cinfo.output_width * cinfo.output_components;
+	uint8_t * pixels = new uint8_t[rowbytes * cinfo.output_height];
+
+	uint8_t ** rows = new uint8_t * [cinfo.output_height];
+	for (unsigned r = 0; r < cinfo.output_height; ++r)
+		rows[r] = pixels + r*rowbytes;
+
+	while (cinfo.output_scanline < cinfo.output_height)
+	{
+		JSAMPARRAY dst = rows + cinfo.output_scanline;  // byte **
+		jpeg_read_scanlines(&cinfo, dst, cinfo.output_height);  // read whole picture
+	}
+
+	w = cinfo.output_width;
+	h = cinfo.output_height;
+
+	// clenup
+	fclose(fin);
+	delete [] rows;
+	jpeg_finish_decompress(&cinfo);
+	jpeg_destroy_decompress(&cinfo);
+
+	return pixels;
+}
+
+void write_image(uint8_t * pixels, size_t w, size_t h, int q, string const & filename)
+{
 	jpeg_compress_struct cinfo;
 
 	jpeg_error_mgr jerr;
@@ -68,9 +117,9 @@ int main(int argc, char * argv[])
 	jpeg_create_compress(&cinfo);
 
 	// Step 2: specify data destination (eg, a file)
-	if ((outfile = fopen(output_filename.c_str(), "wb")) == NULL)
+	if ((outfile = fopen(filename.c_str(), "wb")) == NULL)
 	{
-		fprintf(stderr, "can't open %s\n", output_filename.c_str());
+		fprintf(stderr, "can't open %s\n", filename.c_str());
 		exit(1);
 	}
 
@@ -78,25 +127,25 @@ int main(int argc, char * argv[])
 
 	// Step 3: set parameters for compression
 
-	cinfo.image_width = image_width;      // image width and height, in pixels
-	cinfo.image_height = image_height;
+	cinfo.image_width = w;                // image width and height, in pixels
+	cinfo.image_height = h;
 	cinfo.input_components = 3;           // # of color components per pixel
 	cinfo.in_color_space = JCS_RGB;       // colorspace of input image
 
 	jpeg_set_defaults(&cinfo);
 
-	jpeg_set_quality(&cinfo, quality, TRUE /* limit to baseline-JPEG values */);
+	jpeg_set_quality(&cinfo, q, TRUE /* limit to baseline-JPEG values */);
 
 	// Step 4: Start compressor
 	jpeg_start_compress(&cinfo, TRUE);
 
 	// Step 5: while (scan lines remain to be written)
 	//           jpeg_write_scanlines(...); */
-	row_stride = image_width * 3; // JSAMPLEs per row in image_buffer
+	row_stride = w * 3; // JSAMPLEs per row in image_buffer
 
 	while (cinfo.next_scanline < cinfo.image_height)
 	{
-		row_pointer[0] = &image_buffer[cinfo.next_scanline * row_stride];
+		row_pointer[0] = &pixels[cinfo.next_scanline * row_stride];
 		jpeg_write_scanlines(&cinfo, row_pointer, 1);
 	}
 
@@ -109,11 +158,4 @@ int main(int argc, char * argv[])
 	// Step 7: release JPEG compression object
 
 	jpeg_destroy_compress(&cinfo);
-}
-
-pair<size_t, size_t> im_read(string const & fname, Blob & data)
-{
-	Image im{fname};
-	im.write(&data, "RGB");
-	return make_pair(im.columns(), im.rows());
 }
