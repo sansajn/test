2f02fae2134ddc4d6cc6ae652268d91ee3c146e0
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2017-11-22 23:35:33 +0100

    boost::geometry: working with point

--- boost/geometry/point.cpp
+++ boost/geometry/point.cpp
@@ -1,12 +1,16 @@
+#include <iostream>
 #include <boost/geometry/geometries/point_xy.hpp>
 
-namespace bg = boost::geometry;
+using std::cout;
+using boost::geometry::get;
+using boost::geometry::set;
+using point_xy = boost::geometry::model::d2::point_xy<double>;
 
 int main(int argc, char * argv[])
 {
-	using point_xy = bg::model::d2::point_xy<double>;
-
 	point_xy p{1.0, 2.0};
+	set<0>(p, 3.0);
+	cout << "p = (" << get<0>(p) << ", " << get<1>(p) << ")\n";
 
 	return 0;
 }
