a42740e4626ca5801e6ad1b1fbbb2fbe374b133c
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2017-11-23 00:38:02 +0100

    point aritmetics

--- boost/geometry/point.cpp
+++ boost/geometry/point.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/arithmetic/arithmetic.hpp>
+
+namespace bg = boost::geometry;
 
 using std::cout;
-using boost::geometry::get;
-using boost::geometry::set;
-using point_xy = boost::geometry::model::d2::point_xy<double>;
+using bg::get;
+using bg::set;
+using bg::add_point;
+using bg::subtract_point;
+using point_xy = bg::model::d2::point_xy<double>;
+
 
 int main(int argc, char * argv[])
 {
 	point_xy p{1.0, 2.0};
 	set<0>(p, 3.0);
 	cout << "p = (" << get<0>(p) << ", " << get<1>(p) << ")\n";
-
+	
+	// aritmetics
+	add_point(p, point_xy{2.0, 3.0});
+	subtract_point(p, point_xy{1.0, 2.0});
+	
 	return 0;
 }
