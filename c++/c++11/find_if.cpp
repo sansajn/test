a6937afc42b2a7410d27d0a9b729e7d6b4f5241a
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2018-02-02 16:33:52 +0100

    find_if with bind

--- c++/c++11/find_if.cpp
+++ c++/c++11/find_if.cpp
@@ -2,13 +2,21 @@
 #include <map>
 #include <iostream>
 #include <algorithm>
+#include <functional>
+
+using std::cout;
 
 struct foo
 {
-	typedef std::map<int, int> unit_map;
+	using unit_map = std::map<int, int>;
 	unit_map const & units() const {return _units;}
 	unit_map _units;
 };
+
+bool equal(foo::unit_map::value_type const & val, int expected)
+{
+	return val.second == expected;
+}
 
 int main(int argc, char * argv[])
 {
@@ -16,11 +24,24 @@ int main(int argc, char * argv[])
 	for (int i = 0; i < 10; ++i)
 		f._units.insert(std::make_pair(i, i*i));
 
+	// via lambda
 	auto it = std::find_if(f.units().begin(), f.units().end(),
 		[](foo::unit_map::value_type const & v){return v.second == 2*2;});
 
 	if (it != f.units().end())
 		std::cout << it->first << ":" << it->second << "\n";
+	else
+		cout << "not found\n";
+	
+	// via bind
+	auto it_bind = std::find_if(f.units().begin(), f.units().end(),
+		std::bind(equal, std::placeholders::_1, 2*2));
+		
+	if (it_bind != f.units().end())
+		std::cout << it_bind->first << ":" << it_bind->second << "\n";
+	else
+		cout << "not found\n";
+	
 
 	// by key
 	foo::unit_map const & units = f.units();
