9ab9f22f859b020858ae2284eadee668b3d05207
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2018-02-25 18:01:53 +0100

    agg: render polygon with stroke

--- agg/render_poly.cpp
+++ agg/render_poly.cpp
@@ -9,6 +9,7 @@
 #include <agg_rasterizer_scanline_aa.h>
 #include <agg_path_storage.h>
 #include <agg_conv_curve.h>
+#include <agg_conv_stroke.h>
 
 int const W = 100;
 int const H = W;
@@ -18,6 +19,8 @@ using agg::path_storage;
 using renderer = agg::renderer_base<agg::pixfmt_rgb24>;
 
 void render_polygon(renderer & ren, path_storage & path, rgba8 const & color);
+void render_stroke_polygon(renderer & ren, path_storage & path, rgba8 const & fill_color,
+	rgba8 const & stroke_color, double stroke_width = 1.0);
 
 bool write_ppm(const unsigned char* buf, unsigned width, unsigned height,
 	const char* file_name);
@@ -46,7 +49,7 @@ int main(int argc, char * argv[])
 	path.line_to(90, 90);
 	path.line_to(70, 20);
 	path.close_polygon();
-	render_polygon(ren, path, rgba8{230, 100, 160});
+	render_stroke_polygon(ren, path, rgba8{230, 100, 160}, rgba8{0, 0, 0});
 
 	write_ppm(buffer, W, H, "poly.ppm");
 	delete [] buffer;
@@ -67,6 +70,27 @@ void render_polygon(renderer & ren, path_storage & path, rgba8 const & color)
 	ren_sl.color(color);
 
 	agg::scanline_u8 sl;
+	agg::render_scanlines(ras, sl, ren_sl);
+}
+
+void render_stroke_polygon(renderer & ren, path_storage & path, rgba8 const & fill_color,
+	rgba8 const & stroke_color, double stroke_width)
+{
+	agg::conv_curve<agg::path_storage> curve{path};
+	agg::conv_stroke<agg::conv_curve<agg::path_storage>> stroke{curve};
+	stroke.width(stroke_width);
+
+	agg::renderer_scanline_aa_solid<
+		agg::renderer_base<agg::pixfmt_rgb24> > ren_sl{ren};
+	ren_sl.color(fill_color);
+
+	agg::scanline_u8 sl;
+	agg::rasterizer_scanline_aa<> ras;
+	ras.add_path(curve);
+	agg::render_scanlines(ras, sl, ren_sl);
+
+	ras.add_path(stroke);
+	ren_sl.color(stroke_color);
 	agg::render_scanlines(ras, sl, ren_sl);
 }
 
