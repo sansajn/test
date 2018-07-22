293659351bccda7975191d8ba51b6d492376c5e7
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2018-07-22 21:28:00 +0200

    zmq3: publisher sample

---
+++ zmq3/publisher.cpp
@@ -0,0 +1,32 @@
+// publisher, subscriber pattern
+#include <string>
+#include <thread>
+#include <iostream>
+#include <cassert>
+#include <zmq.h>
+
+using std::string;
+using std::cout;
+
+int main(int argc, char * argv[])
+{
+	string const port = (argc > 1) ? argv[1] : "5557";
+	
+	// publisher
+	void * ctx = zmq_ctx_new();
+	void * pub = zmq_socket(ctx, ZMQ_PUB);
+	string const addr = "tcp://*:" + port;
+	int rc = zmq_bind(pub, addr.c_str());
+	assert(!rc);
+	
+	cout << "broadcasting on " << addr << " ..." << std::endl;
+	
+	while (1)
+	{
+		string const buf{"hello"};
+		zmq_send(pub, buf.data(), buf.size(), 0);
+		std::this_thread::sleep_for(std::chrono::seconds{1});
+	}
+
+	return 0;
+}
