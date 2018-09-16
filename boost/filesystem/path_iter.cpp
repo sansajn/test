06e656275987a5fbb6cf9a37ea1931601359e306
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2018-09-16 10:59:37 +0200

    boost::filesystem: path iteration sample

--- boost/filesystem/path_iter.cpp
+++ boost/filesystem/path_iter.cpp
@@ -7,8 +7,24 @@ namespace fs = boost::filesystem;
 int main(int argc, char * argv[])
 {
 	fs::path p{"/home/ja/temp/data/foo.txt"};
+
+	cout << "for-loop: ";
 	for (fs::path const & e : p)
-		cout << e << "\n";
+		cout << e << ", ";
+
+	cout << "\niterator: ";
+	for (auto it = p.begin(); it != p.end(); ++it)
+		cout << *it << ", ";
+
+	cout << "\nreverse-iterator: ";
+	for (auto it = p.rbegin(); it != p.rend(); ++it)
+		cout << *it << ", ";
+
+	cout << "\nparent_path iteration:\n";
+	for (fs::path x{p}; !x.empty(); x = x.parent_path())
+		cout << "  " << x << "\n";
+
+	cout << "\ndone!\n";
 
 	return 0;
 }
