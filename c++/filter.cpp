6ef4561b4f771243e49e6315caa2bfa514baf773
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2018-10-31 18:25:12 +0100

    improved range filtering

--- c++/filter.cpp
+++ c++/filter.cpp
@@ -9,15 +9,15 @@ template <typename Range, typename Pred>
 class filter
 {
 public:
-	filter(Range const & r, Pred const & p)
+	filter(Range const & r, Pred const & p = Pred{})
 		: _r{r}, _p{p}
 	{}
 
 	class const_iterator
 	{
 	public:
-		const_iterator(typename Range::const_iterator beg, typename Range::const_iterator end, Pred const & pred)
-			: _it{beg}, _end{end}, _p{pred}
+		const_iterator(typename Range::const_iterator beg, filter const & f)
+			: _it{beg}, _f{f}
 		{
 			satisfy_predicate();
 		}
@@ -37,21 +37,27 @@ public:
 	private:
 		void satisfy_predicate()
 		{
-			while (_it != _end && !_p(*_it))
+			while (_it != _f._r.end() && !_f._p(*_it))
 				++_it;
 		}
 
-		typename Range::const_iterator _it, _end;
-		Pred const & _p;
+		typename Range::const_iterator _it;
+		filter const & _f;
 	};
 
-	const_iterator begin() const {return const_iterator{_r.begin(), _r.end(), _p};}
-	const_iterator end() const {return const_iterator{_r.end(), _r.end(), _p};}
+	const_iterator begin() const {return const_iterator{_r.begin(), *this};}
+	const_iterator end() const {return const_iterator{_r.end(), *this};}
 
 private:
 	Range const & _r;
 	Pred const & _p;
 };
+
+template <typename Range, typename Pred>
+filter<Range, Pred> filtered(Range const & r, Pred const & p)
+{
+	return filter<Range, Pred>{r, p};
+}
 
 struct is_positive_number
 {
@@ -62,10 +68,15 @@ int main(int argc, char * argv[])
 {
 	vector<int> v{1, 2, -3, 0, 4, -5};
 
-	auto f = filter<vector<int>, is_positive_number>(v, is_positive_number{});
-	for (int e : f)
-		cout << e << " ";
+	auto f = filter<vector<int>, is_positive_number>(v);
+	for (int n : f)
+		cout << n << " ";
+	cout << "\n";
 
-	cout << "\ndone!";
+	// with lambda
+	for (int n : filtered(v, [](int n){return n > 0;}))
+		cout << n << " ";
+
+	cout << "\ndone!\n";
 	return 0;
 }
