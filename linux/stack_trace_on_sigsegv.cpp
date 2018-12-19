eec9dc800c94cbe0bebfe9ec1dead18d29b10f43
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2018-12-19 15:33:46 +0100

    linux: stack-trace with core dump

--- linux/stack_trace_on_sigsegv.cpp
+++ linux/stack_trace_on_sigsegv.cpp
@@ -2,14 +2,16 @@
 
 $ ./stack_trace_on_sigsegv
 Error: signal 11:
-./stack_trace_on_sigsegv(_Z7handleri+0x2b)[0x400a31]
-/lib/x86_64-linux-gnu/libc.so.6(+0x354b0)[0x7f2aa714e4b0]
-./stack_trace_on_sigsegv(_Z3bazv+0x14)[0x400a8a]
-./stack_trace_on_sigsegv(_Z3barv+0x9)[0x400aa9]
-./stack_trace_on_sigsegv(_Z3foov+0x9)[0x400ab5]
-./stack_trace_on_sigsegv(main+0x23)[0x400adb]
-/lib/x86_64-linux-gnu/libc.so.6(__libc_start_main+0xf0)[0x7f2aa7139830]
-./stack_trace_on_sigsegv(_start+0x29)[0x400939] */
+./stack_trace_on_sigsegv(_Z7handleri+0x2b)[0x400af1]
+/lib/x86_64-linux-gnu/libc.so.6(+0x354b0)[0x7f5a2f0f34b0]
+./stack_trace_on_sigsegv(_Z3bazv+0x14)[0x400b79]
+./stack_trace_on_sigsegv(_Z3barv+0x9)[0x400b98]
+./stack_trace_on_sigsegv(_Z3foov+0x9)[0x400ba4]
+./stack_trace_on_sigsegv(main+0x23)[0x400bca]
+/lib/x86_64-linux-gnu/libc.so.6(__libc_start_main+0xf0)[0x7f5a2f0de830]
+./stack_trace_on_sigsegv(_start+0x29)[0x4009f9]
+Segmentation fault (core dumped) */
+
 #include <stdio.h>
 #include <execinfo.h>
 #include <signal.h>
@@ -19,15 +21,17 @@ Error: signal 11:
 void handler(int sig)
 {
 	void *array[10];
-	size_t size;
 
 	// get void*'s for all entries on the stack
-	size = backtrace(array, 10);
+	size_t size = backtrace(array, 10);
 
 	// print out all the frames to stderr
 	fprintf(stderr, "Error: signal %d:\n", sig);
 	backtrace_symbols_fd(array, size, STDERR_FILENO);
-	exit(1);
+
+	// resend SIGSEGV to get core dump
+	signal(sig, SIG_DFL);
+	kill(getpid(), sig);
 }
 
 void baz()
