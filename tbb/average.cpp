85af44b7cb9c69ef61270b16dbee4ca45625a181
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2019-01-22 23:03:29 +0100

    tbb: average filter sample

---
+++ tbb/average.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+
+using std::cout;
+using namespace tbb;
+
+int const GRAIN_SIZE = 10;
+
+struct average 
+{
+	float * input;
+	float * output;
+	
+	void operator()(blocked_range<int> const & r) const
+	{
+		for (int i = r.begin(); i != r.end(); ++i)
+			output[i] = (input[i-1] + input[i] + input[i+1])*(1/3.0f);
+	}
+};
+
+
+int main(int argc, char * argv[])
+{
+	float input[1024];
+	for (int i = 0; i < 1024; ++i)
+		input[i] = 1.0f * i;
+	float output[1024];
+	int const n = 1024;
+	
+	average avg;
+	avg.input = input+1;
+	avg.output = output;
+	parallel_for(blocked_range<int>{0, n-1, GRAIN_SIZE}, avg);
+	
+	cout << "done!\n";
+	return 0;
+}
