acefd1ef8d7305a92c864955dc3a13a12d98f6dc
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2019-08-27 12:08:04 +0200

    c++: float and double literal comparison sample

---
+++ c++/float_and_literal_comparsion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+
+
+int main(int argc, char * argv[])
+{
+	float f = 0.7;
+	if (f == 0.7)  // comparsion with double
+		cout << "f equals 0.7\n";
+	else
+		cout << "f not equall 0.7\n";
+	
+	cout << "done\n";
+	return 0;
+}
