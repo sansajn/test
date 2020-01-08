7d69b4f8257bad09ac3b8ae6ef5144cb09bfaa91
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-01-08 09:31:31 +0100

    boost::format: formatting float numbers

---
+++ boost/format/float_number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <boost/format.hpp>
+
+using std::cout;
+using boost::format;
+
+int main(int argc, char * argv[])
+{
+	float size1 = (10*1024 + 512)/1024.0f,
+		size2 = 10*1024;
+	cout << format("%1$.1fKB, %2$.1fKB") % size1 % size2 << "\n";
+	return 0;
+}
