662a2f91541d237a17d33be67f305cc996f86d7d
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-02-10 17:42:16 +0100

    linux main

--- game_physics_cookbook/orig/FixedFunctionPrimitives.cpp
+++ game_physics_cookbook/orig/FixedFunctionPrimitives.cpp
@@ -1,8 +1,9 @@
 #define WIN32_LEAN_AND_MEAN 
 #define WIN32_EXTRA_LEAN
-#include <windows.h>
-#include <windowsx.h>
-#include "glad/glad.h"
+// #include <windows.h>
+// #include <windowsx.h>
+// #include "glad/glad.h"
+#include <GLFW/glfw3.h>
 #include <cmath>
 #include "Compare.h"
 #include "FixedFunctionPrimitives.h"
---
+++ game_physics_cookbook/orig/main.cpp
@@ -0,0 +1,68 @@
+#include <chrono>
+#include <iostream>
+#include <cassert>
+#include <GLFW/glfw3.h>
+#include "IWindow.h"
+
+using std::chrono::steady_clock;
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+using std::cerr;
+using std::cout;
+using std::endl;
+
+double GetMilliseconds()
+{
+	return duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count();
+}
+
+void window_size(GLFWwindow * win, int width, int height)
+{
+	IWindow * demo = IWindow::GetInstance();
+	demo->Resize(width, height);
+	if (demo->WasWindowShown())
+		demo->OnResize(width, height);
+}
+
+int main(int argc, char * argv[])
+{
+	if (!glfwInit())
+	{
+		cerr << "fatal, glfw initialization failed" << endl;
+		return 1;
+	}
+	
+	GLFWwindow * w = glfwCreateWindow(640, 480, "glfw window", nullptr, nullptr);
+	assert(w);
+	
+	glfwSetWindowSizeCallback(w, window_size);
+	
+	glfwMakeContextCurrent(w);
+	
+	IWindow * demo = IWindow::GetInstance();
+	assert(demo);
+	
+	demo->OnInitialize();
+	demo->OnResize(640, 480);
+	demo->MarkAsShown();
+	
+	// loop
+	while (!glfwWindowShouldClose(w))
+	{
+		glfwPollEvents();  // process events
+		
+		// update
+		demo->OnUpdate(0.1f);  // dt = 0.1s
+		
+		// render
+		glClear(GL_COLOR_BUFFER_BIT);  // render
+		demo->OnRender();
+		glfwSwapBuffers(w);  // show
+	}
+	
+	demo->Close();
+	demo->OnShutdown();
+	
+	cout << "done!\n";
+	return 0;
+}
