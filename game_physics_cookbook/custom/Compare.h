7690149657f8c45813c1e8a983d89dfdc06fc9d6
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-02-11 20:46:58 +0100

    working

--- game_physics_cookbook/custom/Compare.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cfloat>
-
-
-bool AlmostEqualRelativeAndAbs(float a, float b, float maxDiff, float maxRelDiff = FLT_EPSILON);
-
-#define CMP(x, y) AlmostEqualRelativeAndAbs(x, y, 0.005f)
--- game_physics_cookbook/custom/FixedFunctionPrimitives.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void FixedFunctionOrigin();
-void FixedFunctionOrigin(bool depthTest, bool twoSided);
