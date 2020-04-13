a453343ddc6f8406800273ce0df95e44ac417d5c
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-04-13 10:06:38 +0200

    partial work

--- kmeans/kmeans.cpp
+++ kmeans/kmeans.cpp
@@ -7,6 +7,8 @@
 #include <sstream>
 #include <stdexcept>
 #include <iostream>
+#include <cmath>
+#include <cassert>
 
 using std::count;
 using std::string;
@@ -19,9 +21,11 @@ using std::ifstream;
 using std::istringstream;
 using std::invalid_argument;
 using std::runtime_error;
+using std::pow;
 using std::cout;
 
 using table_type = vector<pair<vector<double>, int>>;
+using cluster_type = pair<size_t, vector<size_t>>;
 
 table_type load_data_from_file(string const & fname);
 void kmeans(table_type const & ds, size_t k);
@@ -33,18 +37,19 @@ double distance(vector<double> const & u, vector<double> const & v)
 
 	double result = 0;
 	for (size_t i = 0; i < size(u); ++i)
-		result = pow(u[i] - v[i]);
+		result += pow(u[i] - v[i], 2);
 
 	return result;
 }
 
+// vrati index zaznam z najvacsou vzdialenostou od centroid
 size_t euclidian_step(table_type const & ds, size_t centroid)
 {
 	size_t max_idx = 0;
 	double max_distance = 0.0;
 	for (size_t i = 0; i < size(ds); ++i)
 	{
-		double dist = eucl_distance(ds[centroid].first); 
+		double dist = distance(ds[centroid].first, ds[i].first); 
 		if (dist > max_distance)
 		{
 			max_idx = i;
@@ -52,6 +57,87 @@ size_t euclidian_step(table_type const & ds, size_t centroid)
 		}
 	}
 	return max_idx;
+}
+
+// get list of initial clusters
+vector<cluster_type> lloyd_step(table_type const & ds, 
+	vector<size_t> const & centroids)
+{
+	vector<cluster_type> clusters(size(centroids));
+	for (size_t i = 0; i < size(centroids); ++i)
+		clusters[i].first = centroids[i];
+
+	// find cluster members
+	for (size_t i = 0; i < size(ds); ++i)
+	{
+		size_t cluster_idx = 0;
+
+		double min_dist = 0;
+		for (size_t j = 0; j < size(clusters); ++j)
+		{
+			double dist = distance(ds[i].first, ds[centroids[j]].first);
+			if (dist < min_dist)
+			{
+				cluster_idx = j;
+				min_dist = dist;
+			}
+		}
+
+		clusters[cluster_idx].second.push_back(i);
+	}
+
+	return clusters;
+}
+
+// \returns list of centroids and list of clusters
+pair<vector<size_t>, vector<cluster_type>> kmeans_plus_plus(
+	table_type const & ds, size_t k)
+{
+	assert(k > 2);  // TODO: implement for k <= 2
+
+	vector<size_t> centroids;
+	centroids.reserve(k);
+
+	// insert randomly selected first centroid
+	centroids.push_back(0);
+
+	// insert second centroid (most far record index)
+	centroids.push_back(euclidian_step(ds, centroids[0]));
+
+	vector<cluster_type> clusters;
+
+	// rest of centroids
+	for (size_t i = k - 2; i > 0; --i)
+	{
+		clusters = lloyd_step(ds, centroids);
+
+		/* for each cluster compute item with the largest distance 
+		to its centroid, which will becomes new cluster centroid */
+		double max_distance = 0;
+		size_t max_cluster_idx, max_item_idx;
+		for (size_t cluster_idx = 0; cluster_idx < size(clusters); ++cluster_idx)
+		{
+			size_t centroid_idx = clusters[cluster_idx].first;
+			
+			vector<size_t> const & cluster_items = clusters[cluster_idx].second;
+			for (size_t item_idx = 0; item_idx < size(cluster_items); ++item_idx)
+			{
+				double dist = distance(ds[centroid_idx].first, 
+					ds[cluster_items[item_idx]].first);
+				
+				if (dist > max_distance)
+				{
+					max_cluster_idx = cluster_idx;
+					max_item_idx = item_idx;
+					max_distance = dist; 
+				}
+			}
+		}
+
+		centroids.push_back(clusters[max_cluster_idx].second[max_item_idx]);
+	}
+
+	return make_pair(centroids, clusters);
 }
 
 void kmeans(table_type const & ds, size_t k)
@@ -65,8 +151,17 @@ int main(int argc, char * argv[])
 	string const dataset_file = argc > 1 ? argv[1] : "dataset.csv";
 	size_t const k = argc > 2 ? atoi(argv[2]) : 5;
 
-	table_type dataset = load_data_from_file(dataset_file);
-	cout << "records: " << size(dataset) << "\n";
+	table_type ds = load_data_from_file(dataset_file);
+	cout << "records: " << size(ds) << "\n";
+
+	// testing kmeans_plus_plus
+	pair<vector<size_t>, vector<cluster_type>> centroids_and_clusters = 
+		kmeans_plus_plus(ds, k);
+
+	cout << "centroids:\n";
+	vector<size_t> const & centroids = centroids_and_clusters.first;
+	for (int centroid : centroids)
+		cout << "  " << centroid << "\n";
 
 	cout << "done!\n";
 	return 0;
