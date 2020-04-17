60821512b53370b16fe61e942cb889a08416c895
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-04-17 10:19:54 +0200

    boost::format: improved hello sample

--- boost/format/hello.cpp
+++ boost/format/hello.cpp
@@ -1,10 +1,18 @@
+/*! \file hello.cpp
+basic formating with boost::format */
+
+#include <string>
 #include <iostream>
 #include <boost/format.hpp>
 
+using std::string;
 using std::cout;
+using boost::format;
+using boost::str;
 
 int main(int argc, char * argv[])
 {
-	cout << boost::format("writing %1%, x=%2% : %3%-th try") % "toto" % 40.23 % 50 << "\n";
+	string s = str(format("hello %1%") % "John");
+	cout << format("writing %1%, x=%2% : %3%-th try") % "toto" % 40.23 % 50 << "\n";
 	return 0;
 }
