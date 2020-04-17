3b53426b45fbd299324d16762da8a0f376496e3e
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-04-17 12:52:48 +0200

    boost::range: improved adaptors sample

--- boost/range/adaptors.cpp
+++ boost/range/adaptors.cpp
@@ -1,3 +1,6 @@
+/*! \file adaptors.cpp
+basic adaptors (map_values, reversed, filtered, ...) sample */
+
 #include <map>
 #include <vector>
 #include <string>
@@ -26,25 +29,25 @@ int main(int argc, char * argv[])
 
 	vector<int> v;
 	boost::copy(m|boost::adaptors::map_values, std::back_inserter(v));
-	print(v);
+	print(v);  // 25, 20, 22, 15, 
 
 	v.clear();
 	vector<int> v1{1, 2, 3, 4, 5};
 	boost::copy(v1|boost::adaptors::reversed, std::back_inserter(v));
-	print(v);
+	print(v);  // 5, 4, 3, 2, 1, 
 
 	v.clear();
 	boost::copy(m|boost::adaptors::filtered(
 			[](map<string, int>::value_type & v){return v.first[0] == 'a';})
 		|boost::adaptors::map_values, std::back_inserter(v));
-	print(v);
+	print(v);  // 25, 20,
 
 	// len pre porovnanie
 	v.clear();
 	for (auto & e : m)
 		if (e.first[0] == 'a')
 			v.push_back(e.second);
-	print(v);
+	print(v);  // 25, 20,
 
 	return 0;
 }
