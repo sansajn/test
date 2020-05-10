09c4eb55e0ea1b0c16311ea24698367f998ba31e
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-05-10 18:06:49 +0200

    latch sample

--- cppcoro/latch.cpp
+++ cppcoro/latch.cpp
@@ -17,6 +17,25 @@ task<> wait_for(async_latch & latch)
 
 int main(int argc, char * argv[])
 {
+	async_latch latch{3};
+	auto waiter = async([&latch]{sync_wait(wait_for(latch));});
+
+	auto counter1 = async([&latch]{
+		this_thread::sleep_for(2s));
+		cout << "counter 1: latch.count_down()" << endl;
+		latch.count_down();
+	});
+
+	auto counter2 = async([&latch]{
+		this_thread::sleep_for(1s);
+		cout << "counter 2: latch.count_down(2)" << endl;
+		latch.count_down(2);
+	});
+
+	waiter.get();
+	counter1.get();
+	counter2.get();
+
 	return 0;
 }
 
