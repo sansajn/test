0897fa2bc5d1135b813232af1114125d3d6c29f2
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-05-10 10:09:24 +0200

    when_all and threadpool samples

--- cppcoro/thread_pool.cpp
+++ cppcoro/thread_pool.cpp
@@ -1,7 +1,11 @@
-// waiting for the results with when_all function sample
+// thread pool sample
 #include <chrono>
 #include <string>
 #include <iostream>
+#include <cppcoro/sync_wait.hpp>
+#include <cppcoro/task.hpp>
+#include <cppcoro/static_thread_pool.hpp>
+#include <cppcoro/when_all.hpp>
 
 using std::chrono::duration_cast, std::chrono::steady_clock;
 using namespace std::chrono_literals;
@@ -26,16 +30,29 @@ cppcoro::task<string> third()
 	co_return "Third";
 }
 
+template <typename Func>
+cppcoro::task<string> run_on_thread_pool(cppcoro::static_thread_pool & tp, Func func)
+{
+	co_await tp.schedule();
+	auto res = co_await func();
+	co_return res;
+}
+
 cppcoro::task<> run_all()
 {
-	auto [fir, sec, thi] = co_await cppcoro::when_all(first(), second(), third());
+	auto [fir, sec, thi] = co_await cppcoro::when_all(
+		run_on_thread_pool(tp, first), 
+		run_on_thread_pool(tp, second),
+		run_on_thread_pool(tp, third));
 	cout << fir << " " << sec << " " << thi << endl;
 }
 
 int main(int argc, char * argv[])
 {
 	auto start = steady_clock::now();
-	cppcoro::sync_wait(run_all());
+	cppcoro::static_thread_pool tp;
+	cppcoro::sync_wait(run_all(tp));
 	cout << "execution time " << duration_cast<double>(steady_clock::now() - start).count() << "s\n";
 	return 0;
 }
+
