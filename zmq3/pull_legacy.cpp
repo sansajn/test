cce8867a45d75c0ccb82f3306be5a4c1a1850a6a
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-09-24 13:41:19 +0200

    partial work

--- zmq3/pull_legacy.cpp
+++ zmq3/pull_legacy.cpp
@@ -1,4 +1,4 @@
-// pull pattern sample with monitored ZMQ events, see `push_mon.cpp` for push pattern sample
+// pull pattern sample with monitored ZMQ events (for legacy ZMQ 4.14 and C++11 running on debian8), see `push_mon.cpp` for push pattern sample
 #include <string>
 #include <thread>
 #include <iostream>
@@ -6,30 +6,34 @@
 #include <chrono>
 #include <zmq.h>
 
-using std::string, std::to_string;
-using std::cout, std::endl;
-using namespace std::chrono_literals;
+using std::string;
+using std::to_string;
+using std::cout;
+using std::endl;
+using std::chrono::seconds;
+
+string const DEFAULT_HOST = "*",
+	DEFAULT_PORT = "5557";
 
 constexpr char const * MONITOR_ADDR = "inproc://pull_monitor";
 
 static string zmq_event_to_string(int event);
 
-
 int main(int argc, char * argv[])
 {
-	string const host = (argc > 1) ? argv[1] : "localhost";
-	string const port = (argc > 2) ? argv[2] : "5557";
+	string const host = (argc > 1) ? argv[1] : DEFAULT_HOST;
+	string const port = (argc > 2) ? argv[2] : DEFAULT_PORT;
 	
 	// pull
 	void * ctx = zmq_ctx_new();
 	void * pull = zmq_socket(ctx, ZMQ_PULL);
-	string const addr = "tcp://*:" + port;
-	int rc = zmq_bind(pull, addr.c_str());
-	assert(!rc);
+	string const addr = "tcp://" + host + ":" + port;
+	int res = zmq_bind(pull, addr.c_str());
+	assert(!res);
 
 	// create pull monitor
-	rc = zmq_socket_monitor(pull, MONITOR_ADDR, ZMQ_EVENT_ALL);
-	assert(rc >= 0);
+	res = zmq_socket_monitor(pull, MONITOR_ADDR, ZMQ_EVENT_ALL);
+	assert(res >= 0);
 
 	// connect to monitor
 	void * pull_mon = zmq_socket(ctx, ZMQ_PAIR);
@@ -38,7 +42,7 @@ int main(int argc, char * argv[])
 	
 	cout << "listenning on " << addr << " ..." << std::endl;
 
-	std::this_thread::sleep_for(1s);  // wait for ZMQ to bind (otherwise we do not receive first ZMQ message)
+	std::this_thread::sleep_for(seconds{1});  // wait for ZMQ to bind (otherwise we do not receive first ZMQ message)
 	
 	zmq_pollitem_t items[] = {
 		{pull, 0, ZMQ_POLLIN, 0},
@@ -47,8 +51,8 @@ int main(int argc, char * argv[])
 
 	while (true)
 	{
-		rc = zmq_poll(&items[0], 2, 20);  // 20ms poll
-		assert(rc != -1);
+		res = zmq_poll(&items[0], 2, 20);  // 20ms poll
+		assert(res != -1);
 
 		if (items[0].revents & ZMQ_POLLIN)
 		{
@@ -62,15 +66,15 @@ int main(int argc, char * argv[])
 			// we expect two frame message (see zmq_socket_monitor() description)
 			zmq_msg_t msg;
 			zmq_msg_init(&msg);
-			rc = zmq_msg_recv(&msg, pull_mon, 0);
-			assert(rc != -1);
+			res = zmq_msg_recv(&msg, pull_mon, 0);
+			assert(res != -1);
 
 			assert(zmq_msg_more(&msg));
 			uint16_t event = *(uint16_t *)zmq_msg_data(&msg);
 			cout << "event: " << zmq_event_to_string(event) << ", ";
 
-			rc = zmq_msg_recv(&msg, pull_mon, 0);
-			assert(rc != -1);
+			res = zmq_msg_recv(&msg, pull_mon, 0);
+			assert(res != -1);
 			cout << string((char *)zmq_msg_data(&msg), zmq_msg_size(&msg)) << endl;
 
 			assert(!zmq_msg_more(&msg));  // last message
@@ -97,9 +101,9 @@ string zmq_event_to_string(int event)
 		case ZMQ_EVENT_CLOSE_FAILED: return "close failed";
 		case ZMQ_EVENT_DISCONNECTED: return "disconnected";
 		case ZMQ_EVENT_MONITOR_STOPPED: return "monitor stopped";
-		case ZMQ_EVENT_HANDSHAKE_FAILED_NO_DETAIL: return "handshake failed";
-		case ZMQ_EVENT_HANDSHAKE_SUCCEEDED: return "handshake succeeded";
-		case ZMQ_EVENT_HANDSHAKE_FAILED_PROTOCOL: return "handshake failed (protocol)";
+//		case ZMQ_EVENT_HANDSHAKE_FAILED_NO_DETAIL: return "handshake failed";
+//		case ZMQ_EVENT_HANDSHAKE_SUCCEEDED: return "handshake succeeded";
+//		case ZMQ_EVENT_HANDSHAKE_FAILED_PROTOCOL: return "handshake failed (protocol)";
 		default: return "unknown (" + to_string(event) + ")";
 	}
 }
