9c594762848a96b0e26bd63d2e7219797ec5b69d
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-11-30 14:20:43 +0100

    program_options: sample with optional arguments

---
+++ boost/program_options/optional_with_default.cpp
@@ -0,0 +1,43 @@
+/* two optional options --encoder NAME (with h264 as default), --page PAGE
+usage: ./optional_with_default [--encoder h264|vp8][--page PATH] */
+#include <string>
+#include <iostream>
+#include <boost/program_options.hpp>
+
+using std::cout, std::string;
+namespace po = boost::program_options;
+
+int main(int argc, char * argv[])
+{
+	string arg_encoder;
+
+	po::options_description desc{"commandline options"};
+	desc.add_options()
+		("help", "produce help messages")
+		("encoder", po::value<string>(&arg_encoder)->default_value("h264"), "video encoder")
+		("page", po::value<string>(), "use external html page to show video content");
+
+	po::positional_options_description pos_desc;
+
+	// process arguments
+	po::variables_map vm;
+	po::store(po::command_line_parser(argc, argv).options(desc).positional(pos_desc).run(), vm);
+	po::notify(vm);
+	
+	if (vm.count("help"))
+	{
+		cout << desc << "\n";
+		return 1;
+	}
+
+	if (vm.count("page"))
+		cout << "external page defined: " << vm["page"].as<string>() << "\n";
+	else
+		cout << "page not defined, default page used\n";
+
+	cout << "encoder: " << arg_encoder << "\n";
+
+
+	cout << "done!\n";
+	return 0;
+}
