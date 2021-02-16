4da01e7bb0a0ebfe039fdc83142455b5910809f3
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2021-02-16 17:20:41 +0100

    removes unused code

--- bullet/solid_objects.cpp
+++ bullet/solid_objects.cpp
@@ -311,58 +311,6 @@ void draw_scene()
 
 	glLoadIdentity();				// make sure we're no longer rotated.
 
-/*
-	glTranslatef(cube_position[0], cube_position[1], cube_position[2]);
-	
-	glRotatef(rquad,1.0f,1.0f,1.0f);		// Rotate The Cube On X, Y, and Z
-
-	// draw a cube (6 quadrilaterals)
-	glBegin(GL_QUADS);				// start drawing the cube.
-
-	// top of cube
-	glColor3f(0.0f,1.0f,0.0f);			// Set The Color To Blue
-	glVertex3f( 1.0f, 1.0f,-1.0f);		// Top Right Of The Quad (Top)
-	glVertex3f(-1.0f, 1.0f,-1.0f);		// Top Left Of The Quad (Top)
-	glVertex3f(-1.0f, 1.0f, 1.0f);		// Bottom Left Of The Quad (Top)
-	glVertex3f( 1.0f, 1.0f, 1.0f);		// Bottom Right Of The Quad (Top)
-
-	// bottom of cube
-	glColor3f(1.0f,0.5f,0.0f);			// Set The Color To Orange
-	glVertex3f( 1.0f,-1.0f, 1.0f);		// Top Right Of The Quad (Bottom)
-	glVertex3f(-1.0f,-1.0f, 1.0f);		// Top Left Of The Quad (Bottom)
-	glVertex3f(-1.0f,-1.0f,-1.0f);		// Bottom Left Of The Quad (Bottom)
-	glVertex3f( 1.0f,-1.0f,-1.0f);		// Bottom Right Of The Quad (Bottom)
-
-	// front of cube
-	glColor3f(1.0f,0.0f,0.0f);			// Set The Color To Red
-	glVertex3f( 1.0f, 1.0f, 1.0f);		// Top Right Of The Quad (Front)
-	glVertex3f(-1.0f, 1.0f, 1.0f);		// Top Left Of The Quad (Front)
-	glVertex3f(-1.0f,-1.0f, 1.0f);		// Bottom Left Of The Quad (Front)
-	glVertex3f( 1.0f,-1.0f, 1.0f);		// Bottom Right Of The Quad (Front)
-
-	// back of cube.
-	glColor3f(1.0f,1.0f,0.0f);			// Set The Color To Yellow
-	glVertex3f( 1.0f,-1.0f,-1.0f);		// Top Right Of The Quad (Back)
-	glVertex3f(-1.0f,-1.0f,-1.0f);		// Top Left Of The Quad (Back)
-	glVertex3f(-1.0f, 1.0f,-1.0f);		// Bottom Left Of The Quad (Back)
-	glVertex3f( 1.0f, 1.0f,-1.0f);		// Bottom Right Of The Quad (Back)
-
-	// left of cube
-	glColor3f(0.0f,0.0f,1.0f);			// Blue
-	glVertex3f(-1.0f, 1.0f, 1.0f);		// Top Right Of The Quad (Left)
-	glVertex3f(-1.0f, 1.0f,-1.0f);		// Top Left Of The Quad (Left)
-	glVertex3f(-1.0f,-1.0f,-1.0f);		// Bottom Left Of The Quad (Left)
-	glVertex3f(-1.0f,-1.0f, 1.0f);		// Bottom Right Of The Quad (Left)
-
-	// Right of cube
-	glColor3f(1.0f,0.0f,1.0f);			// Set The Color To Violet
-	glVertex3f( 1.0f, 1.0f,-1.0f);	        // Top Right Of The Quad (Right)
-	glVertex3f( 1.0f, 1.0f, 1.0f);		// Top Left Of The Quad (Right)
-	glVertex3f( 1.0f,-1.0f, 1.0f);		// Bottom Left Of The Quad (Right)
-	glVertex3f( 1.0f,-1.0f,-1.0f);		// Bottom Right Of The Quad (Right)
-	glEnd();					// Done Drawing The Cube
-*/
-
 	world.native().debugDrawWorld();
 
 	// swap the buffers to display, since double buffering is used.
