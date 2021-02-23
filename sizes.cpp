62412a2af3c454b1dbb89cfcc5689294bfa24835
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2021-02-23 11:49:54 +0100

    missing size for unsigned type

--- sizes.cpp
+++ sizes.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-using std::cout;
 #include <cstddef>
+using std::cout;
 
 
-int main()
+int main(int argc, char * argv[])
 {
 	cout << "bool:" << sizeof(bool) << "\n"
 		<< "char:" << sizeof(char) << "\n"
 		<< "wchar_t:" << sizeof(wchar_t) << "\n"
 		<< "short:" << sizeof(short) << "\n"
 		<< "int:" << sizeof(int) << "\n"
+		<< "unsigned:" << sizeof(unsigned) << "\n"
 		<< "long:" << sizeof(long) << "\n"
 		<< "long long:" << sizeof(long long) << "\n"
 		<< "float:" << sizeof(float) << "\n"
