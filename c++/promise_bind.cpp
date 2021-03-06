1cc389831a86566657fac14ecd4392ff3345d961
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2021-03-06 08:16:16 +0100

    c++: binding promise in async like function sample

---
+++ c++/promise_bind.cpp
@@ -0,0 +1,35 @@
+// binding promise in async like function sample
+#include <functional>
+#include <future>
+#include <thread>
+#include <utility>
+#include <iostream>
+using std::promise, std::future;
+using std::thread;
+using std::move;
+using std::cout, std::endl;
+
+int meaning_of_life() {return 42;}
+
+template<typename F>
+auto foo(F && f) -> future<decltype(f())>
+{
+	using result_type = decltype(f());
+	promise<result_type> p;
+	future<result_type> result = p.get_future();
+
+	thread{[result = move(p), fce = move(f)]() mutable {
+		result.set_value(fce());
+	}}.detach();
+
+	return result;
+}
+
+int main(int argc, char * argv[])
+{
+	auto answer = foo([]{return meaning_of_life();});
+	cout << "the answer is " << answer.get() << "\n";
+
+	cout << "done\n";
+	return 0;
+}
