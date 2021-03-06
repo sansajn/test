7e1e502d0e32cd275e2f45fdf489ecefa7422596
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2021-03-06 08:11:14 +0100

    c++: run multiple async tasks sample

--- c++/async.cpp
+++ c++/async.cpp
@@ -1,8 +1,10 @@
 // async() sample (at least C++11 required)
 #include <future>
+#include <vector>
 #include <iostream>
-using std::cout;
+using std::vector;
 using std::async, std::future;
+using std::cout;
 
 int meaning_of_life()
 {
@@ -11,9 +13,19 @@ int meaning_of_life()
 
 int main(int argc, char * argv[])
 {
+	// run one worker
 	future<int> answer = async(meaning_of_life);
 	cout << "The answer is " << answer.get() << "\n";
 	
+	// run more workers
+	vector<future<int>> results;
+	for (int i = 0; i< 5; ++i)
+		results.push_back(async(meaning_of_life));
+
+	for (auto & r : results)
+		cout << r.get() << ", ";
+	cout << "\n";
+
 	cout << "done!\n";
 	return 0;
 }
