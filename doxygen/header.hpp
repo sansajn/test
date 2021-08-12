883012897893e39ce126d9027dcfdb583daaf8a1
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2021-08-12 11:14:15 +0200

    doxygen: header file comment sample

---
+++ doxygen/header.hpp
@@ -0,0 +1,11 @@
+/*! \file 
+\brief Just an example header file.
+\author Adam Hlavatovič
+\version 20110514
+\date 2008-2011
+\bug Bad memory allocation when ...
+\warning This class may explode when you use it.
+
+More detailed header description goes there. 
+
+\sa std::string std::vector std::list foo */
