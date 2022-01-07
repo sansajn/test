cab5f10b1a52a2851a244ed0a05172cef0d436bf
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2022-01-07 12:19:19 +0100

    gcrypt: sha512 sample

--- gcrypt/fsha2_512.cpp
+++ gcrypt/fsha2_512.cpp
@@ -15,8 +15,7 @@ using std::ifstream;
 namespace fs = boost::filesystem;
 
 
-string to_hex(unsigned char * s, size_t len)
-{
+string to_hex(unsigned char * s, size_t len) {
 	ostringstream hex_out;
 	for (size_t i = 0; i < len; ++i)
 		hex_out << std::hex << std::setw(2) << std::setfill('0') << (int)s[i];
@@ -24,30 +23,27 @@ string to_hex(unsigned char * s, size_t len)
 	return hex_out.str();
 }
 
-string md5sum(fs::path const & in)
-{
+string sha2_512sum(fs::path const & in) {
 	ifstream fin{in.string().c_str()};
 	if (!fin.is_open())
 		return string{};
 
 	gcry_md_hd_t hd;
-	gcry_error_t err = gcry_md_open(&hd, GCRY_MD_MD5, 0);
-	if (err)
-	{
+	gcry_error_t err = gcry_md_open(&hd, GCRY_MD_SHA512, 0);
+	if (err) {
 		cerr << "LibGCrypt error " << gcry_strsource(err) << "/" << gcry_strerror(err) << std::endl;
-		return string{};
+		return {};
 	}
 
 	unsigned char buf[4096];
-	while (fin)
-	{
+	while (fin) {
 		fin.read((char *)buf, sizeof(buf));
 		gcry_md_write(hd, buf, fin.gcount());
 	}
 
 	fin.close();
 
-	string result = to_hex(gcry_md_read(hd, 0), gcry_md_get_algo_dlen(GCRY_MD_MD5));
+	string result = to_hex(gcry_md_read(hd, 0), gcry_md_get_algo_dlen(GCRY_MD_SHA512));
 
 	gcry_md_reset(hd);
 	gcry_md_close(hd);
@@ -56,11 +52,10 @@ string md5sum(fs::path const & in)
 }
 
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
 	fs::path in = argc > 1 ? argv[1] : argv[0];
 
-	cout << md5sum(in) << "\t" << in << "\n";
+	cout << sha2_512sum(in) << "\t" << in << "\n";
 
 	cout << "done!\n";
 
