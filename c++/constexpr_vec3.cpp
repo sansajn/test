b4e790bcd37f0886ce3c9dd440a1be5a416320b0
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2022-02-11 07:24:20 +0100

    c++: nodiscard sample

--- c++/constexpr_vec3.cpp
+++ c++/constexpr_vec3.cpp
@@ -1,16 +1,23 @@
-// constexpr 3D vector sample implementation
+// nodiscard constexpr 3D vector sample implementation
 
 struct vec3 {
 	double x, y, z;
+
+	constexpr vec3(double d) : x{d}, y{d}, z{d} {}
 
 	constexpr vec3(double x, double y, double z)
 		: x{x}, y{y}, z{z}
 	{}
 
-	constexpr vec3 operator+(vec3 const & v) const {return {x+v.x, y+v.y, z+v.z};}
-	constexpr vec3 operator-(vec3 const & v) const {return {x-v.x, y-v.y, z-v.z};}
-	constexpr vec3 operator*(double k) const {return {k*x, k*y, k*z};}
-	// ...
+	[[nodiscard]] friend constexpr vec3 operator+(vec3 const & u, vec3 const & v) {return {u.x+v.x, u.y+v.y, u.z+v.z};}
+	[[nodiscard]] friend constexpr vec3 operator-(vec3 const & u, vec3 const & v) {return {u.x-v.x, u.y-v.y, u.z-v.z};}
+
+	[[nodiscard]] constexpr vec3 operator*(double k) const {return {k*x, k*y, k*z};}
+	[[nodiscard]] friend constexpr vec3 operator*(double k, vec3 const & v) {return v*k;}
+	[[nodiscard]] friend constexpr vec3 operator*(vec3 const & u, vec3 const & v) {return u.dot(v);}
+
+	[[nodiscard]] constexpr vec3 dot(vec3 const & v) const {return {x*v.x, y*v.y, z*v.z};}
+	// cross()
 };
 
 int main(int argc, char * argv[]) {
@@ -18,7 +25,8 @@ int main(int argc, char * argv[]) {
 		v{2, 2, 0},
 		k = u + v,
 		l = u - v,
-		m = u*10.0;
+		m = u*10.0,
+		n = 10*v;
 
 	return 0;
 }
