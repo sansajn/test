192927ac314409c71ac3faff45df14c4c4928cac
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2022-04-07 09:06:20 +0200

    input iterator skeleton

--- c++/iterator/input_it.cpp
+++ c++/iterator/input_it.cpp
@@ -4,6 +4,8 @@
 #include <iterator>
 #include <cmath>
 #include <cstdint>
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
 #include "png.hpp"
 using std::transform, std::pair, std::begin, std::end;
 
@@ -21,27 +23,56 @@ struct pixel_pos_view
 		return _pos;
 	}
 
+	pair<size_t, size_t> const * operator->() {
+		return &_pos;
+	}
+
+	void operator++() {}
+
+	void operator++(int) {}
+
+	bool operator==(pixel_pos_view const & rhs) {
+		return false;
+	}
+
+	bool operator!=(pixel_pos_view const & rhs) {
+		return !(*this == rhs);
+	}
+
 private:
 	size_t _w, _h;
 	pair<size_t, size_t> _pos;
 };
 
 
+TEST_CASE("input iterator should allow following expressions", 
+	"[input-iterator]") {
+	
+	pixel_pos_view pos1, pos2;
+	*pos1;  // access position as (x,y) pair
+	pos1->first;  // access x
+	++pos1;  // pre increment
+	pos1++;  // post increment
+	pos1 == pos2;  // equal operator
+	pos1 != pos2;  // not equal operator
+	pixel_pos_view pos3{pos1};  // copy constructor
+}
+
+/*
 int main(int argc, char * argv[]) {
 	constexpr size_t w = 400,
 		h = 300;
 
 	// input iterator requirements
 	pixel_pos_view pos1, pos2;
-	// *pos1;  // access position as (x,y) pair
-	// pos1->first;  // access x
-	// ++pos1;  // pre increment
-	// pos1++;  // post increment
-	// pos1 == pos2;  // equal operator
-	// pos1 != pos2;  // not equal operator
-	// pixel_pos_view pos3{pos1};  // copy constructor
+	*pos1;  // access position as (x,y) pair
+	pos1->first;  // access x
+	++pos1;  // pre increment
+	pos1++;  // post increment
+	pos1 == pos2;  // equal operator
+	pos1 != pos2;  // not equal operator
+	pixel_pos_view pos3{pos1};  // copy constructor
 
-/*
 	// input iterator final test
 	uint8_t pixels[w*h];  // grayscale pixels 
 	pixel_pos_view pos{w, h};
@@ -54,8 +85,7 @@ int main(int argc, char * argv[]) {
 		});
 
 	save_grayscale_png("input.png");
-*/
 
 	return 0;
 }
-
+*/
