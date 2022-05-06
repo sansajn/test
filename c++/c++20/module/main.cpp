78a81a2c92de4100c68d60beed685ed3500396db
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2022-05-06 07:49:54 +0200

    c++20: module interface unit sample

--- c++/c++20/module/main.cpp
+++ c++/c++20/module/main.cpp
@@ -1,6 +1,7 @@
-// C++20 one file module sample from https://blog.feabhas.com/2021/08/c20-modules-with-gcc11/
+// single file C++20 module sample from https://blog.feabhas.com/2021/08/c20-modules-with-gcc11/
 import mod;
 
 int main() {
 	func();
+	return 0;
 }
