22222d01554e8639680fb90181501369793de2fc
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2022-06-03 14:05:17 +0200

    boost: core modeule sample skeleton

---
+++ boost/core/span.cpp
@@ -0,0 +1,11 @@
+/* boost::span<> sample from core module
+see [span](https://www.boost.org/doc/libs/1_79_0/libs/core/doc/html/core/span.html) 
+
+\note span implementation available from boost 1.78 (not even available in Ubuntu 22.04 LTS) */
+#include <boost/core/span.hpp>
+
+void print() {}
+
+int main(int argc, char * argv[]) {
+	return 0;
+}
