b6af284a9a79814732a075fc70500632eca5f033
Adam Hlavatovic <adh@ciklum.com>
2023-07-13 03:29:52 -0400

    soap: websocket server page reload test sample (with spdlog)

--- libsoup/websocket_reload.cpp
+++ libsoup/websocket_reload.cpp
@@ -1,20 +1,22 @@
-// server with websocket connection sample
+// server with websocket support (and handling connection closing) sample
 #include <string>
 #include <thread>
 #include <chrono>
+#include <string_view>
 #include <iostream>
 #include <cassert>
 #include <boost/filesystem/string_file.hpp>
+#include <spdlog/spdlog.h>
 #include <libsoup/soup.h>
 
-using std::string, std::cout, std::endl;
+using std::string_view, std::string, std::cout, std::endl;
 using namespace std::chrono_literals;
 using boost::filesystem::load_string_file;
 
 constexpr int SERVER_PORT = 40001;
-constexpr char const * default_page_file = "websocket.html";
+constexpr char const * default_page_file = "websocket_reload.html";
 
-string page_source;
+string page_source;  // content of page-file
 
 void server_http_handler(SoupServer * server, SoupMessage * message,
 	char const * path, GHashTable * query, SoupClientContext * client, gpointer user_data);
@@ -22,8 +24,11 @@ void server_http_handler(SoupServer * server, SoupMessage * message,
 void server_websocket_handler(SoupServer * server, SoupWebsocketConnection * connection,
 	char const * path, SoupClientContext * client, gpointer user_data);
 
+void websocket_closed_handler(SoupWebsocketConnection * connection, gpointer user_data);
+
 void websocket_message_handler(SoupWebsocketConnection * connection,
 	SoupWebsocketDataType data_type, GBytes * message, gpointer user_data);
+
 
 int main(int argc, char * argv[]) {
 	string const page_file = argc > 1 ? argv[1] : default_page_file;
@@ -54,8 +59,8 @@ int main(int argc, char * argv[]) {
 }
 
 void server_http_handler(SoupServer * server, SoupMessage * message,
-	char const * path, GHashTable * query, SoupClientContext * client, gpointer user_data)
-{
+	char const * path, GHashTable * query, SoupClientContext * client, gpointer user_data) {
+	
 	cout << "http request received" << endl;
 
 	SoupBuffer * page_buf = soup_buffer_new(SOUP_MEMORY_STATIC, page_source.c_str(), size(page_source));
@@ -70,40 +75,41 @@ void server_http_handler(SoupServer * server, SoupMessage * message,
 }
 
 void server_websocket_handler(SoupServer * server, SoupWebsocketConnection * connection,
-	char const * path, SoupClientContext * client, gpointer user_data)
-{
+	char const * path, SoupClientContext * client, gpointer user_data) {
+
 	cout << "websocket connection request received" << endl;
 
 	g_object_ref(G_OBJECT(connection));
+	g_signal_connect(G_OBJECT(connection), "closed", G_CALLBACK(websocket_closed_handler), nullptr);
 	g_signal_connect(G_OBJECT(connection), "message", G_CALLBACK(websocket_message_handler), nullptr);
 }
 
-void websocket_message_handler(SoupWebsocketConnection * connection,
-	SoupWebsocketDataType data_type, GBytes * message, gpointer user_data)
-{
-	gchar * msg_str = nullptr;  // received message as string
+void websocket_closed_handler(SoupWebsocketConnection * connection, gpointer user_data) {
+	gushort close_code = soup_websocket_connection_get_close_code(connection);
+	spdlog::info("client disconnected, what: {}", close_code);
+	g_object_unref(connection);
+}
 
-	switch (data_type)
-	{
-		case SOUP_WEBSOCKET_DATA_BINARY:
-		{
+void websocket_message_handler(SoupWebsocketConnection * connection,
+	SoupWebsocketDataType data_type, GBytes * message, gpointer user_data) {
+
+	switch (data_type) {
+		case SOUP_WEBSOCKET_DATA_BINARY: {
 			cout << "websocket: unknown binary message received, ignored" << endl;
 			return;
 		}
 
-		case SOUP_WEBSOCKET_DATA_TEXT:
-		{
+		case SOUP_WEBSOCKET_DATA_TEXT: {
 			gsize size;
 			gchar * data = (gchar *)g_bytes_get_data(message, &size);
-			msg_str = g_strndup(data, size);
+			string_view str{data, size};
+
+			cout << "ws >> " << str << endl;
+		
 			break;
 		}
 
 		default:
 			assert(0);
 	}
-
-	cout << "ws >> " << msg_str << endl;
-
-	g_free(msg_str);
 }
