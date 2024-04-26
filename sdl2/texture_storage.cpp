d2eda55df2e67628364cff601ef92249c410155e
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2024-04-26 17:53:54 +0200

    SDL rendering texture sample

---
+++ sdl2/texture_storage.cpp
@@ -0,0 +1,193 @@
+// rendering texture with glTexStorage2D() sample
+#include <string>
+#include <cassert>
+#include <iostream>
+#include <SDL.h>
+#include <GLES3/gl32.h>
+#include <Magick++.h>
+
+using std::cout, std::endl;
+
+std::string const texture_path = "lena.jpg";
+
+constexpr GLuint WIDTH = 800,
+	HEIGHT = 600;
+
+char const * vs_src = R"(
+#version 320 es
+in vec3 position;
+out vec2 st;
+void main() {
+	st = position.xy/2.0 + 0.5;
+	gl_Position = vec4(position, 1.0f);
+})";
+
+char const * fs_src = R"(
+#version 320 es
+precision mediump float;
+uniform sampler2D s;
+in vec2 st;
+out vec4 frag_color;
+void main() {
+	frag_color = texture(s, st);
+})";
+
+void create_mesh();
+GLuint create_texture(std::string const & fname);
+GLint get_shader_program(char const * vertex_shader_source, char const * fragment_shader_source);
+
+GLuint vs, fs;
+GLuint vao, vbo, ibo;
+unsigned index_count;
+
+int main(int argc, char * argv[]) {
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* window = SDL_CreateWindow("OpenGL ES 3.2", SDL_WINDOWPOS_UNDEFINED, 
+		SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	cout << "GL_VENDOR: " << glGetString(GL_VENDOR) << "\n" 
+		<< "GL_VERSION: " << glGetString(GL_VERSION) << "\n"
+		<< "GL_RENDERER: " << glGetString(GL_RENDERER) << "\n"
+		<< "GLSL_VERSION: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+	
+	GLuint const shader_program = get_shader_program(vs_src, fs_src);
+	
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	create_mesh();
+	GLuint tbo = create_texture(texture_path);
+
+	// rendering ...
+	glUseProgram(shader_program);
+
+	GLuint position_attr_id = 0;
+	glVertexAttribPointer(position_attr_id, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(position_attr_id);
+
+	GLint s_loc = glGetUniformLocation(shader_program, "s");
+	assert(s_loc != -1 && "unknown uniform");
+	glUniform1i(s_loc, 0);
+	glActiveTexture(GL_TEXTURE0);
+
+	while (true) {
+		SDL_Event event;
+		if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+			break;
+
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+		glDrawElements(GL_TRIANGLES, index_count, GL_UNSIGNED_INT, 0);
+		assert(glGetError() == GL_NO_ERROR && "opengl error");
+
+		SDL_GL_SwapWindow(window);
+	}
+
+	glDeleteTextures(1, &tbo);
+	glDeleteBuffers(1, &vbo);
+	glDeleteBuffers(1, &ibo);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+	glDeleteProgram(shader_program);
+	glDeleteVertexArrays(1, &vao);
+
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
+
+GLuint create_texture(std::string const & fname) {
+	Magick::Image im{fname};
+	im.flip();
+	Magick::Blob imblob;
+	im.write(&imblob, "RGBA");  // load image as rgba array
+
+	GLuint tbo;
+	glGenTextures(1, &tbo);
+	glBindTexture(GL_TEXTURE_2D, tbo);
+	// glPixelStorei() : adresa kazdeho riadku obrazka je zarovnana 4mi (RGBA foramt)
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, im.columns(), im.rows());
+	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, im.columns(), im.rows(), GL_RGBA, GL_UNSIGNED_BYTE, imblob.data());
+
+	return tbo;
+}
+
+void create_mesh()
+{
+	GLfloat vertices[] = {
+		-1, -1, 0,
+		 1, -1, 0,
+		 1,  1, 0,
+		-1,  1, 0};
+
+	GLuint indices[] = {
+		0, 1, 2,  2, 3, 0
+	};
+
+	index_count = sizeof(indices)/sizeof(GLuint);
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 4*3*sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6*sizeof(GLuint), indices, GL_STATIC_DRAW);
+}
+
+GLint get_shader_program(char const * vertex_shader_source, char const * fragment_shader_source) {
+	enum Consts {INFOLOG_LEN = 512};
+	GLchar infoLog[INFOLOG_LEN];
+	GLint fragment_shader;
+	GLint shader_program;
+	GLint success;
+	GLint vertex_shader;
+
+	/* Vertex shader */
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+	glCompileShader(vertex_shader);
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertex_shader, INFOLOG_LEN, NULL, infoLog);
+		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" 
+			<< infoLog << endl;
+	}
+
+	/* Fragment shader */
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+	glCompileShader(fragment_shader);
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragment_shader, INFOLOG_LEN, NULL, infoLog);
+		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" 
+			<< infoLog << endl;
+	}
+
+	/* Link shaders */
+	shader_program = glCreateProgram();
+	glAttachShader(shader_program, vertex_shader);
+	glAttachShader(shader_program, fragment_shader);
+	glLinkProgram(shader_program);
+	glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shader_program, INFOLOG_LEN, NULL, infoLog);
+		cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" 
+			<< infoLog << endl;
+	}
+
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+	return shader_program;
+}
