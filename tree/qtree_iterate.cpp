1b70abe9629930879dbfd52f19f1b4829a2ad349
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2024-12-31 15:10:56 +0100

    simple iterate algorithm

--- tree/qtree_iterate.cpp
+++ tree/qtree_iterate.cpp
@@ -1,1 +1,49 @@
 // iterating quad tree sample
+#include <array>
+#include <iostream>
+#include <cstddef>
+using std::array;
+using std::cout;
+
+// quad tree
+struct node {
+	array<node *, 4> children = {0};
+	int data = -1;
+	bool is_leaf() const {return children[0] == nullptr;}
+};
+
+void iterate(node const & root) {
+	if (root.is_leaf())
+		cout << root.data << ", ";
+	else {
+		iterate(*root.children[0]);
+		iterate(*root.children[1]);
+		iterate(*root.children[2]);
+		iterate(*root.children[3]);
+	}
+}
+
+int main(int argc, char * argv[]) {
+	// build a simple tree
+	array<node, 8> pool = {0};
+	for (size_t i = 0; i < size(pool); ++i) // TODO: is there any better way to initialize data?
+		pool[i].data = i+1;
+
+	node root;
+	root.children[0] = &pool[0];
+	root.children[1] = &pool[1];  // leaf
+	root.children[2] = &pool[2];  // leaf
+	root.children[3] = &pool[3];  // leaf
+
+	node & r00 = *root.children[0];
+	r00.children[0] = &pool[4];  // leaf
+	r00.children[1] = &pool[5];  // leaf
+	r00.children[2] = &pool[6];  // leaf
+	r00.children[3] = &pool[7];  // leaf
+
+	// iterare tree
+	iterate(root);  //= 5, 6, 7, 8, 2, 3, 4
+	cout << '\n';
+
+	return 0;
+}
