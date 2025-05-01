c8baa3a792f144264d9b3e95414aeabaae5bef32
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2025-05-01 16:57:11 +0200

    small improvements

--- c++/ranges/count.cpp
+++ c++/ranges/count.cpp
@@ -1,12 +1,14 @@
 // count algorithm sample
 #include <algorithm>
 #include <vector>
+#include <cassert>
 #include <iostream>
 using std::vector, std::cout;
 
 int main(int argc, char * argv[]) {
 	vector d = {1,2,3,1,1};
-	int count_1 = std::ranges::count(d, 1);  //= 3
+	int const count_1 = std::ranges::count(d, 1);  //= 3
+	assert(count_1 == 3);
 	cout << "count_1=" << count_1 << '\n';
 	return 0;
 }
